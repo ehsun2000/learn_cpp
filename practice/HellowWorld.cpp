a92d93eb707345c533afb5b2542bdb12cb62a5b0
charles.zheng <charles.zheng@imsoft.com.tw>
2025-09-25 12:02:34 +0800

    feat: continue when user press double enter and delete track build file

--- practice/HellowWorld.cpp
+++ practice/HellowWorld.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <limits>
 
 int main(int argc, char const *argv[])
 {
     std::cout << "Hello World!\n";
+
+    // Wait for user input before closing the console window
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+
     return 0;
 }
